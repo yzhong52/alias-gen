7c7917ecbcba74d90984a55ba12b12ad905f1814
Yuchen Zhong <yzhong.cs@gmail.com>
2018-12-11 22:49:57 -0500

    Fix invalid folder path

--- alias_gen.py
+++ alias_gen.py
@@ -5,7 +5,7 @@ from pathlib import Path
 # ~/.alias_gen
 alias_gen_file = Path.home().joinpath(".alias_gen") 
 # ~/Documents/
-workspace = Path.home().joinpath("/Documents/")
+workspace = Path.home().joinpath("Documents/")
 
 sys.stdout=open(str(alias_gen_file), "w+")
 
@@ -16,10 +16,12 @@ print("#")
 for root, folders, files in os.walk(workspace):
     for folder in sorted(folders):
         try:
-            _ = git.Repo(folder).git_dir # May throw
+            _ = git.Repo(workspace.joinpath(folder)).git_dir # May throw
             alias = ''.join(folder.split()).lower()
             absolute_path=os.path.abspath(os.path.join(root, folder))
             print('alias ' + alias + '="cd ' + absolute_path + '"')
         except git.exc.InvalidGitRepositoryError:
             print("# Skiping directory '" + folder + "' because it is not a git repo")
+
+    # breaking right away since we only care about one level deep
     break
